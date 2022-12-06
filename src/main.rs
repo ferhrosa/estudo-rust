5da2c48ae5e3e5bfd8ecf35fdea98bb4ff787c05
Fernando H. Rosa <ferhrosa@gmail.com>
2022-12-05 23:27:31 -0300

    Exercício de enums

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("Olá mundo!");
     vetor();
     matriz();
+    enums();
 }
 
 fn vetor() {
@@ -22,15 +23,85 @@ fn vetor() {
     }
 }
 
-fn matriz(){
-    let matriz: [[f32; 3]; 2] = [
-        [0.0, 1.2, 0.1],
-        [1.3, 0.3, 1.4],
-    ];
+fn matriz() {
+    let matriz: [[f32; 3]; 2] = [[0.0, 1.2, 0.1], [1.3, 0.3, 1.4]];
 
     for linha in matriz {
         for item in linha {
             println!("Item = {}", item);
         }
     }
+}
+
+fn enums() {
+    let dia_da_semana = DiaDaSemana::Terca;
+    println!("É final de semana? {}", eh_final_de_semana(dia_da_semana));
+
+    cores();
+}
+
+#[allow(dead_code)]
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado,
+}
+
+fn eh_final_de_semana(dia_da_semana: DiaDaSemana) -> bool {
+    match dia_da_semana {
+        DiaDaSemana::Domingo | DiaDaSemana::Sabado => true,
+        _ => false,
+    }
+}
+
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8, u8, u8),
+    CmykColor {
+        cyan: u8,
+        magenta: u8,
+        yellow: u8,
+        black: u8,
+    },
+}
+
+fn cores() {
+    // let cor = Color::Blue;
+    // let cor = Color::RgbColor(255, 255, 255);
+    let cor = Color::CmykColor {
+        cyan: 20,
+        magenta: 30,
+        yellow: 40,
+        black: 50,
+    };
+
+    println!(
+        "Cor = {}",
+        match cor {
+            Color::Red => "vermelho",
+            Color::Green => "verde",
+            Color::Blue => "azul",
+            Color::RgbColor(0, 0, 0) => "preto",
+            Color::RgbColor(_, _, _)
+            | Color::CmykColor {
+                cyan: _,
+                magenta: _,
+                yellow: _,
+                black: 255,
+            } => "RGB desconhecido",
+            Color::CmykColor {
+                cyan: _,
+                magenta: _,
+                yellow: _,
+                black: _,
+            } => "CMYK desconhecido",
+        }
+    );
 }
